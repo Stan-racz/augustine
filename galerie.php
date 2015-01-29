044d06271d0bdf65e8196ec913c8e77059f84ae0
Mathieu-S <mathieusartorio@gmail.com>
2015-01-29 10:37:27 +0100

    Modification de la page gallery avec affichage .js (Lightbox ) pour les photos

--- galerie.php
+++ galerie.php
@@ -1,70 +1,64 @@
-<?php require_once('inc/bdd_conf.php'); 
-      require_once('inc/fonctions.php');
-    require_once('inc/header.php');
+<?php
+require_once('inc/bdd_conf.php');
+require_once('inc/fonctions.php');
+require_once('inc/header.php');
+require_once('inc/menu.php');
 ?>
+<html>
 
-<body>
+<link href="css/lightbox.css" rel="stylesheet" />
 
-    <?php require_once('inc/menu.php'); ?>
+<!-- JavaScript -->
+<script src="js/jquery-1.10.2.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/lightbox.min.js"></script>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Galerie</h1>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Galerie</h1>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="container">
-
-        <div class="row">
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug1.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug1.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug1.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug2.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug2.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug3.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug3.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug4.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug4.jpg"></a>
+                </div>
             </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug2.jpg">
+            <br>
+            <div class="row">
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug5.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug5.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug6.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug6.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug7.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug7.jpg"></a>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                        <a class="example-image-link" href="img/aug8.jpg" data-lightbox="example-set"><img class="img-responsive" src="img/aug8.jpg"></a>
+                </div>
             </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug3.jpg">
-            </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug4.jpg">
-            </div>
-
-        </div>
-<BR>
-        <div class="row">
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug5.jpg">
-            </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug6.jpg">
-            </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug7.jpg">
-            </div>
-
-            <div class="col-md-3 portfolio-item">
-                    <img class="img-responsive" src="img/aug8.jpg">
-            </div>
-
         </div>
 
-    </div>
+        <!-- /.container -->
 
-    <!-- /.container -->
 
-    <!-- JavaScript -->
-    <script src="js/jquery-1.10.2.js"></script>
-    <script src="js/bootstrap.js"></script>
 
-</body>
+    </body>
 <?php require_once('inc/footer.php'); ?>
 </html>
