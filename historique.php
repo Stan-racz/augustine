2248b5cd730d639c7f144248ee13a90600578e5f
Mathieu-S <mathieusartorio@gmail.com>
2015-02-11 16:20:18 +0100

    Petite correction

--- historique.php
+++ historique.php
@@ -25,39 +25,39 @@ include_once('inc/menu.php');
         </article>
         <article class="bs-docs-section" id="2012">
             <h2 class="page-header">Année 2012</h2>
-                <p class="container">
-                    Second projet également en partenariat avec le Lycée Diderot. Il s'agit de la première version d'Augustine.
-                    <br>
-                    <br>
-                    <img src="img/2012.png" alt="Photo 2012" class="img-rounded">
-                    <br>
-                    <br>
-                    Cette année çi, le challenge était quel que peut différent.
-                    Il ne fallait pas faire 17,14 km mais 25 km pour une vitesse moyenne minimum de 30 km/h.
-                    Les efforts des étudiants ont pu être récompensés car ils on réussis à ce classer 31ième sur 56.
-                </p>
+            <p class="container">
+                Second projet également en partenariat avec le Lycée Diderot. Il s'agit de la première version d'Augustine.
+                <br>
+                <br>
+                <img src="img/2012.png" alt="Photo 2012" class="img-rounded">
+                <br>
+                <br>
+                Cette année çi, le challenge était quel que peut différent.
+                Il ne fallait pas faire 17,14 km mais 25 km pour une vitesse moyenne minimum de 30 km/h.
+                Les efforts des étudiants ont pu être récompensés car ils on réussis à ce classer 31ième sur 56.
+            </p>
         </article>
         <article class="bs-docs-section" id="2013">
-            <h2 class="page-header">Année 2013</h2>
-                <p class="container">
-                    Toisième projet sous le nom d'Augustine II.
-                    <br>
-                    <br>
-                    <aside class="row">
-                        <img src="img/2013.jpg" alt="Photo 2013" class="img-responsive">
-                    </aside>
-                    <br>
-                    <br>
-                    La grande soeur d'Augstine à su tenir ses promesses car elle a remportée pas moins de 5 trophés:
-                    <br>
-                    <ul>
-                        <li>Le grand prix du prototype</li>
-                        <li>Le prix de la modélisation et de la simulation numérique</li>
-                        <li>Le 1er prix du mémoire de projet</li>
-                        <li>Le 2ème prix de l'éducation Nationale</li>
-                        <li>Le 1er prix de la sécurité</li>
-                    </ul>
-                </p>
+           <h2 class="page-header">Année 2013</h2>
+              <p class="container">
+                   Toisième projet sous le nom d'Augustine II.
+                   <br>
+                   <br>
+                   <aside class="row">
+                       <img src="img/2013.jpg" alt="Photo 2013" class="img-responsive">
+                   </aside>
+                   <br>
+                   <br>
+                   La grande soeur d'Augstine à su tenir ses promesses car elle a remportée pas moins de 5 trophés:
+                   <br>
+                   <ul>
+                       <li>Le grand prix du prototype</li>
+                       <li>Le prix de la modélisation et de la simulation numérique</li>
+                       <li>Le 1er prix du mémoire de projet</li>
+                       <li>Le 2ème prix de l'éducation Nationale</li>
+                       <li>Le 1er prix de la sécurité</li>
+                   </ul>
+               </p>
         </article>
         <article class="bs-docs-section" id="presse">
             <h2 class="page-header">Presse</h2>
