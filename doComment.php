a54059be8ab8b5b6d28cd69c2a549be1083e18ea
Stan-racz <stan.racz7@gmail.com>
2015-03-05 10:28:30 +0100

    Checking du remplissage des champs, puis redirection si champs manquants

--- doComment.php
+++ doComment.php
@@ -4,32 +4,42 @@
 
     //récupération des données du commentaire
     $cpt=0;
-    if(isset($_POST['com_user'])){
+    if(!empty($_POST['com_user'])){
         $nom_user=$_POST['com_user'];
         $cpt++;
     }
-    if(isset($_POST['com_note'])){
+    if(!empty($_POST['com_note'])){
         $note_com=$_POST['com_note'];
         $cpt++;
     }
-    if(isset($_POST['com'])){
+    if(!empty($_POST['com'])){
         $com=$_POST['com'];
         $cpt++;
     }
+    if(!empty($_POST['idactu'])){
+        $idActu=$_POST['idactu'];
+        $cpt++;
+    }
     //execution de la requete si condition ok
-    if($cpt==3){
-        $id=$pdo->lastInsertId();
-        $actu=getLastActualite();
+    if($cpt==4){
+
         $req=$pdo->prepare("INSERT INTO `commentaire`(`id`, `pseudo`, `texte`, `note`, `ip`, `idActu`) VALUES (:id, :nom, :com, :note, :ip, :idactu)");
         $req->bindParam(':id', $id);
         $req->bindParam(':nom', $nom_user);
         $req->bindParam(':com', $com);
         $req->bindParam(':note', $note_com);
         $req->bindParam(':ip', $_SERVER['REMOTE_ADDR']);
-        $req->bindParam(':idactu', $actu['id']);
+        $req->bindParam(':idactu', $idActu);
         $req->execute();
+
         //Affichage de l'alerte commentaire bien enregistré
-        print('<body onLoad="alert(\'Commentaire bien enregistre\')">');
+        print('<body onLoad="alert(\'Commentaire bien enregistré.\')">');
+        //Redirection vers la page d'actualité
+        print('<meta http-equiv="refresh" content="0;URL=actualite.php">');
+    }
+    else {
+        //Affichage de l'alerte commentaire wrong
+        print('<body onLoad="alert(\'Champs manquants : erreur.\')">');
         //Redirection vers la page d'actualité
         print('<meta http-equiv="refresh" content="0;URL=actualite.php">');
     }
